c83a2634245d971f3e4355520d91ead768b08e03
neatstranger <39034037+neatstranger@users.noreply.github.com>
2022-12-07 05:49:27 -0700

    Added a couple of extra things

--- pi_script.py
+++ pi_script.py
@@ -27,8 +27,12 @@ color_two = (0,16,0)
 color_three = (4,4,4)
 off = (0, 0, 0)
 
+garage_section = 130
+kitchen_section = 209
+
 pixels.fill((0,0,0))
 while True:
+    pixels.auto_write = True
     while (datetime.datetime.now().hour >= 17 and datetime.datetime.now().hour <= 23) or (datetime.datetime.now().hour >= 4 and datetime.datetime.now().hour <= 7) :
         red = 0
         while red < 10:
@@ -43,6 +47,7 @@ while True:
             pixels.fill((white, white, white))
             white += 0.1
         x = 0
+
         while x <= pixelCount:
             print(x)
             red = 0 
@@ -54,6 +59,23 @@ while True:
                pixels[x+1] = (0,green,0)
                green +=2
             x += 2
+        pixels.auto_write = False
+
+        for x in range(0,25):
+            for pixel in range(0,garage_section):
+                pixels[pixel] = (0,0,x)
+            for pixel in range(garage_section, kitchen_section):
+                pixels[pixel] = (0,x,0)
+            pixels.show()
+            time.sleep(0.5)
+        for x in range(0,25):
+            for pixel in range(0,garage_section):
+                pixels[pixel] = (0,25,0)
+            for pixel in range(garage_section, kitchen_section):
+                pixels[pixel] = (0,0,25)
+            pixels.show()
+            time.sleep(0.5)
+        
         time.sleep(10)
         
 
